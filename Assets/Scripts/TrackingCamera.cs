e642269c80aac3d17f70d481ff0f2c6f8ecffa05
Peter Rolfe <30848697+PeteyWoufes@users.noreply.github.com>
2022-10-02 14:15:35 +0100

    Tracking camera works

--- Assets/Scripts/TrackingCamera.cs
+++ Assets/Scripts/TrackingCamera.cs
@@ -1,18 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrackingCamera : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+public class TrackingCamera : MonoBehaviour {
+    public float camera_height;
+    public float rounding_factor = 5f;
+    public Transform player_transform;
+    
+    public void Update() {
+        camera_height = Mathf.Round(player_transform.position.y * rounding_factor) / rounding_factor;
+        transform.position = new Vector3(0, camera_height, -10);
     }
 }
